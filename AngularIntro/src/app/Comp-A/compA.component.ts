19a629283a47890d524920cfe63ab7b52320c884
hajifullstack@02 <haji.fullstack.com>
2025-10-02 00:29:10 +0530

    master angular

---
+++ AngularIntro/src/app/Comp-A/compA.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+@Component(
+    {
+        selector    : 'Comp-A',
+        templateUrl : './compA.component.html',
+        styleUrls   : ['./compA.component.css']
+    }
+)
+export class CompAComponent{
+    
+}
