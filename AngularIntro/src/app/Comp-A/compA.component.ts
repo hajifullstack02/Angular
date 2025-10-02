54c62f9cb72c0af77018f77e390d2ad035b88fea
hajifullstack@02 <haji.fullstack.com>
2025-10-02 22:46:28 +0530

    databinding

--- AngularIntro/src/app/Comp-A/compA.component.ts
+++ AngularIntro/src/app/Comp-A/compA.component.ts
@@ -1,3 +1,4 @@
+import { style } from "@angular/animations";
 import { Component } from "@angular/core";
 @Component(
     {
@@ -8,4 +9,27 @@ import { Component } from "@angular/core";
 )
 export class CompAComponent{
     
+
+  // Variables for interpolation & property binding
+  title = 'Angular Data Binding Example';
+  imageUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
+  isDisabled = false;
+ 
+
+  // Variable for two-way binding
+  username = 'Haji';
+  // spl-btn="greeen";
+  // Method for event binding
+  onButtonClick() {
+    // spl-btn ="red";
+    alert(`Hello ${this.username}, you clicked the button!`);
+  }
+
+  // Method to toggle image
+  toggleImage() {
+    this.imageUrl = this.imageUrl.includes('angular.png')
+      ? 'https://angular.io/assets/images/logos/angular/angular_solidBlack.png'
+      : 'https://angular.io/assets/images/logos/angular/angular.png';
+  }
+ 
 }
--- AngularIntro/src/app/app.module.ts
+++ AngularIntro/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {CompAComponent} from '../app/Comp-A/compA.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import {CompAComponent} from '../app/Comp-A/compA.component';
     CompAComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
